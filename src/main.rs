1d1eb4375944ab1ad57e8b649c4d8125a619d0ad
Andrew <mcdude22@gmail.com>
2023-05-24 22:51:46 -0700

    Loops implemented but messy

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,16 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let content = fs::read_to_string(&args[1]).expect("Did not find file");
+    let chars: Vec<char> = content.chars().collect();
     const MEM_SIZE: usize = 30;
 
     let mut mem: [u8; MEM_SIZE] = [0; MEM_SIZE];
     let mut index = 0;
     let mut loop_count = 0;
 
-    for c in content.chars(){
-        match c{
+    let mut i = 0;
+    while i < chars.len(){
+        match chars[i]{
             '>' => if index < MEM_SIZE{index += 1},
             '<' => if index > 0{index -= 1},
             '+' => mem[index] += 1,
@@ -20,10 +22,44 @@ fn main() {
             '[' => {
                 if mem[index] != 0 {
                     loop_count += 1;
+                }else{
+                    let mut temp_count = 0;
+                    let mut skip = 0;
+                    for x in i+1..chars.len(){
+                        skip += 1;
+                        if chars[x] == '['{
+                            temp_count += 1;
+                        }else if chars[x] == ']'{
+                            if temp_count == 0 {
+                                i += skip;
+                                break;
+                            }else{
+                                temp_count -= 1;
+                            }
+                        }
+                    }
                 }
-            }
+            },
+            ']' => {
+                if mem[index] != 0 {
+                    let mut temp_count = 0;
+                    for x in 1..i {
+                        if chars[i-x] == ']'{
+                            temp_count += 1;
+                        }else if chars[i-x] == '['{
+                            if temp_count == 0{
+                                i -= x;
+                                break;
+                            }else{
+                                temp_count -= 1;
+                            }
+                        }
+                    }
+                }
+            },
             _ => {},
         }
+        i+=1;
     }
-    // println!("{:?}", mem);
+    println!("{:?}", mem);
 }
