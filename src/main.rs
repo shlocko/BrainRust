cb52002d3c289045eee77b805586236b0cf34c3b
Andrew <mcdude22@gmail.com>
2023-05-24 22:08:37 -0700

    BF basics working, need loop and input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::fs;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let content = fs::read_to_string(&args[1]).expect("Did not find file");
+    const MEM_SIZE: usize = 30;
+
+    let mut mem: [u8; MEM_SIZE] = [0; MEM_SIZE];
+    let mut index = 0;
+    let mut loop_count = 0;
+
+    for c in content.chars(){
+        match c{
+            '>' => if index < MEM_SIZE{index += 1},
+            '<' => if index > 0{index -= 1},
+            '+' => mem[index] += 1,
+            '-' => mem[index] -= 1,
+            '.' => println!("{}", mem[index] as char),
+            '[' => {
+                if mem[index] != 0 {
+                    loop_count += 1;
+                }
+            }
+            _ => {},
+        }
+    }
+    // println!("{:?}", mem);
 }
